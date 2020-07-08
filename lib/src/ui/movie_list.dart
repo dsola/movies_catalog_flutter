eb672c7d439dfc9bab45adba853fb34ad13f3b28
David Sola <david@thecloseapp.com>
2020-07-08 16:15:20 +0200

    Include navigation within the app

--- lib/src/ui/movie_list.dart
+++ lib/src/ui/movie_list.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import '../models/item_model.dart';
 import '../blocs/movies_bloc.dart';
+import 'movie_detail.dart';
 
 class MovieList extends StatefulWidget {
+  @override
   State<StatefulWidget> createState() {
-    return new MovieListState();
+    return MovieListState();
   }
 }
 
@@ -23,7 +25,6 @@ class MovieListState extends State<MovieList> {
 
   @override
   Widget build(BuildContext context) {
-    bloc.fetchAllMovies();
     return Scaffold(
       appBar: AppBar(
         title: Text('Popular Movies'),
@@ -48,11 +49,33 @@ class MovieListState extends State<MovieList> {
         gridDelegate:
         new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
-          return Image.network(
-            'https://image.tmdb.org/t/p/w185${snapshot.data
-                .results[index].poster_path}',
-            fit: BoxFit.cover,
+          return GridTile(
+            child: InkResponse(
+              enableFeedback: true,
+              child: Image.network(
+                'https://image.tmdb.org/t/p/w185${snapshot.data
+                    .results[index].poster_path}',
+                fit: BoxFit.cover,
+              ),
+              onTap: () => openDetailPage(snapshot.data, index),
+            ),
           );
         });
+  }
+
+  openDetailPage(ItemModel data, int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return MovieDetail(
+          title: data.results[index].title,
+          posterUrl: data.results[index].backdrop_path,
+          description: data.results[index].overview,
+          releaseDate: data.results[index].release_date,
+          voteAverage: data.results[index].vote_average.toString(),
+          movieId: data.results[index].id,
+        );
+      }),
+    );
   }
 }
